28c6fecbe4572451fd35fb660bab3f1e6dc03e23
naga-sai-raghu-garimella <146939564+naga-sai-raghu-garimella@users.noreply.github.com>
2023-10-26 18:31:43 +0530

    Update TodoList

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -2,16 +2,30 @@ import React, { useContext } from "react";
 import { UserContext } from "./Routing";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import TodoTasks from "./TodoTasks";
 
 //displaying name after sign up with correct data submition without any errors
 const TodoList = () => {
   const { user } = useContext(UserContext);
   return (
-    <Box className="mt-8">
-      <Typography variant="h5" className="text-green-700 text-center">
-        Welcome {user.firstname} {user.lastname}
-      </Typography>
-    </Box>
+    <>
+      <Box className="bg-sky-300 min-h-screen p-8">
+        <Box className="pt-16">
+          <Typography
+            variant="h5"
+            sx={{
+              fontSize: "30px",
+              fontWeight: "bold",
+              color: "green",
+              textAlign: "center",
+            }}
+          >
+            Welcome {user.firstname} {user.lastname}
+          </Typography>
+        </Box>
+        <TodoTasks />
+      </Box>
+    </>
   );
 };
 
